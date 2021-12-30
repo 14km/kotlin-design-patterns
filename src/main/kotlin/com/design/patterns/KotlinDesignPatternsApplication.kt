7288c756ad77f9085487c98f0bbbf1881e983c09
14km <k.connor614@gmail.com>
2021-12-30 22:23:22 +0900

    git add

---
+++ src/main/kotlin/com/design/patterns/KotlinDesignPatternsApplication.kt
@@ -0,0 +1,11 @@
+package com.design.patterns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDesignPatternsApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinDesignPatternsApplication>(*args)
+}
